1df12fec645098859008690e1a8c73ab8ee166b6
Ivan Winslow <ivan.winslow@errepar.com>
2023-02-06 13:31:52 -0300

    redirect and token at cookie check

--- domain2/src/App2.jsx
+++ domain2/src/App2.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react';
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App2() {
-
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   //LISTENER TO RECEIBE TOKEN FROM D1
-window.addEventListener('message', function(event) {
-  console.log("EVENTO",event)
-  if(event.data.event_id === 'my_cors_message'){
-      console.log(event.data.data);
-  }
-});
+  window.addEventListener('message', function (event) {
+    console.log(event.data.token);
+    setToken(event.data.token);
+    document.cookie = `token_key_d2=${event.data.token}; Secure`;
+    setLoading(false);
+  });
 
-// console.log("APP2 LOCALSTORAGE", localStorage.getItem("token"));
+  useEffect(() => {
+    const tokenFromCookies = document.cookie.split(';')[0].split('=')[1];
+
+    if (!tokenFromCookies) {
+      setTimeout(() => {
+        window.location.replace(
+          'http://localhost:7700/application?applicationId=1'
+        );
+      }, 3000);
+    }
+  }, []);
 
   return (
     <div className="App">
-      <iframe src="http://localhost:5173/"  style={{display:'none'}}></iframe>
       <div>DOMAIN 2</div>
-      {
-        token?(
+      {loading ? (
+        <>
+          <iframe
+            src="http://localhost:7700/"
+            style={{ display: 'none' }}
+          ></iframe>
+          <div>Loading ...</div>
+        </>
+      ) : token ? (
+        <>
           <div>ALREADY LOGGED</div>
-        ):(
+        </>
+      ) : (
+        <>
           <div>YOU ARE NOT LOGGED! will be redirect to login</div>
-        )
-      }
-      
-     
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App2
+export default App2;
