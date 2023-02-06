fad5231a66f1f8f462ee62a99a0a61cdebc4f63a
Ivan Winslow <ivan.winslow@errepar.com>
2023-02-06 13:32:44 -0300

    redirect and token at cookie check

--- domain1/src/App1.jsx
+++ domain1/src/App1.jsx
@@ -1,53 +1,67 @@
-
 import { useEffect, useState } from 'react';
-import './App.css'
+import './App.css';
 
 function App1() {
+  const [token, setToken] = useState(null);
 
-  const [token, setToken] = useState(null)
-
-
-  useEffect(()=>{
+  useEffect(() => {
     if (document.cookie && !token) {
       //get token from cookies
-      const tokenFromCookies = document.cookie[0].split(";")[0].split("=")[1]
-      setToken(tokenFromCookies)
+      const tokenFromCookies = document.cookie.split(';')[0].split('=')[1];
+      setToken(tokenFromCookies);
       //send it to application
-      if(window.parent[0]){
+      if (window.parent[0]) {
         //control ancestorOrigins[0] === http://127.0.0.1:5500/ (application d2)
-        window.parent[0].location.ancestorOrigins[0]==="http://127.0.0.1:5500"&&window.parent.postMessage(
-          {token:tokenFromCookies}, 
-           "http://127.0.0.1:5500/"
-       ); 
-        
+        window.parent[0].location.ancestorOrigins[0] ===
+          'http://127.0.0.1:5500' &&
+          window.parent.postMessage(
+            { token: tokenFromCookies },
+            'http://127.0.0.1:5500/'
+          );
       }
-      
     }
-  },[]);
+  }, [document.cookie]);
 
-  const setTokenCookie = ()=>{
-    document.cookie = "token_key_d1=tokenD1; SameSite=None; Secure";
-    //document.cookie = `token_key_d1=tokenD1; Secure`;
-    //localStorage.setItem("token", `token_key_d1=tokenD1`)
-    setToken("tokenD1")
-  }
-  
+  useEffect(() => {
+    if (token) {
+      const applicationToRedirect = window.location.href
+        .slice(
+          window.location.href.indexOf('applicationId'),
+          window.location.href.indexOf('applicationId') + 15
+        )
+        .split('=')[1];
+
+      applicationToRedirect === '1' &&
+        window.location.replace('http://127.0.0.1:5500');
+    }
+  }, [token]);
+
+  const setTokenCookie = () => {
+    document.cookie = 'token_key_d1=tokenGlobal; SameSite=None; Secure';
+
+    setToken('tokenGlobal');
+    const applicationToRedirect = window.location.href
+      .slice(
+        window.location.href.indexOf('applicationId'),
+        window.location.href.indexOf('applicationId') + 15
+      )
+      .split('=')[1];
+    applicationToRedirect === '1' &&
+      window.location.replace('http://127.0.0.1:5500');
+  };
 
   return (
     <div className="App">
       <div>DOMAIN 1</div>
-     <div>
-      {
-        token?(
+      <div>
+        {token ? (
           <div>ALREADY LOGGED</div>
-        ):(
+        ) : (
           <button onClick={setTokenCookie}> LOGIN </button>
-        )
-      }
-      
-     </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App1
+export default App1;
